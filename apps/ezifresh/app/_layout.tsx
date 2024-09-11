0388ace4c27f30f3ac5a1e4520543778be0f5496
Victor Ndaba <mwangindaba@gmail.com>
2024-09-11 18:36:03 +0300

    chore: scaffold ezifresh in monorepo

---
+++ apps/ezifresh/app/_layout.tsx
@@ -0,0 +1,28 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import Bootsplash from "react-native-bootsplash";
+import "react-native-reanimated";
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      Bootsplash.hide({ fade: true });
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+}
