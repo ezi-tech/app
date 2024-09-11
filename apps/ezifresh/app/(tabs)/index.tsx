6a826d2c4c94916353a880b5babcfd94110ff261
Emilio Kariuki <emilio113kariuki@gmail.com>
2024-09-11 22:39:40 +0300

    feat: adds react native reusables

--- apps/ezifresh/app/(tabs)/index.tsx
+++ apps/ezifresh/app/(tabs)/index.tsx
@@ -1,10 +1,18 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Button } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text className="text-red-500 text-3xl">This is the home screen</Text>
+      <Button
+        onPress={() => {
+          console.log("Button clicked");
+        }}
+      >
+        <Text className="text-green-700 font-bold">Click me</Text>
+      </Button>
     </SafeAreaView>
-  )
+  );
 }
