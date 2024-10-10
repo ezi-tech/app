68e908d11404e446f609fa5bca24a8f55a7f68aa
Victor Ndaba <mwangindaba@gmail.com>
2024-10-10 10:53:08 +0300

    feat: add initial schema

--- apps/web/lib/africastalking.ts
+++ apps/web/lib/africastalking.ts
@@ -1,15 +1,13 @@
-const url = `https://api.africastalking.com/version1/messaging`;
-
 export async function sendSMS(phone: string, message: string) {
-  const apikey = process.env.AT_API_KEY;
-  const username = process.env.AT_USERNAME;
+  const apikey = process.env.AT_API_KEY || "atsk_363ddc11f7ff5687efa4d9247e20cae642ab7a785f89b1066f64791598a0f1274a9ea43b";
+  const username = process.env.AT_USERNAME || "ezitech";
   const senderId = process.env.AT_SENDER_ID || "EZITECH";
 
   if (!apikey || !username) {
     throw new Error("Missing AT_API_KEY or AT_USERNAME environment variable");
   }
 
-  return fetch(url, {
+  return fetch(`https://api.africastalking.com/version1/messaging`, {
     method: "POST",
     headers: {
       apikey,
--- packages/database/src/schema.ts
+++ packages/database/src/schema.ts
@@ -2,9 +2,11 @@ import { createId } from "@paralleldrive/cuid2";
 import { relations } from "drizzle-orm";
 import {
   boolean,
+  decimal,
   geometry,
   index,
   pgTable,
+  primaryKey,
   text,
   timestamp,
 } from "drizzle-orm/pg-core";
@@ -63,21 +65,30 @@ export const Region = pgTable(
       withTimezone: true,
     }).$onUpdate(() => new Date()),
     name: text("name").notNull(),
+    locationCenter: geometry("location_center", {
+      type: "point",
+      mode: "xy",
+      srid: 4326,
+    }).notNull(),
     // north east viewport bound
-    locationNE: geometry("locationNE", {
+    locationNE: geometry("location_north_east", {
       type: "point",
       mode: "xy",
       srid: 4326,
     }).notNull(),
     // south west viewport bound
-    locationSW: geometry("locationSW", {
+    locationSW: geometry("location_south_west", {
       type: "point",
       mode: "xy",
       srid: 4326,
     }).notNull(),
   },
   (t) => ({
-    spatialIndex: index("spatial_index1").using("gist", t.locationNE, t.locationSW),
+    spatialIndex: index("spatial_index").using(
+      "gist",
+      t.locationNE,
+      t.locationSW,
+    ),
   }),
 );
 
@@ -86,6 +97,7 @@ export const AddressRelations = relations(Address, ({ one }) => ({
 }));
 
 export const User = pgTable("user", {
+  // clerk will provide the user id
   id: text("id").primaryKey().notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updatedAt", {
@@ -100,6 +112,155 @@ export const User = pgTable("user", {
 export const userRelations = relations(User, ({ many }) => ({
   addresses: many(Address),
 }));
+
+export const Store = pgTable(
+  "store",
+  {
+    // clerk will provide the org id
+    id: text("id").primaryKey().notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt", {
+      mode: "date",
+      withTimezone: true,
+    }).$onUpdate(() => new Date()),
+    // store location and find region on the fly
+    location: geometry("location", {
+      type: "point",
+      mode: "xy",
+      srid: 4326,
+    }).notNull(),
+    name: text("name").notNull(),
+    regionId: text("region_id")
+      .notNull()
+      .references(() => Region.id),
+    description: text("description"),
+    legalName: text("legal_name").notNull(),
+    ownerId: text("store_owner_id")
+      .notNull()
+      .references(() => User.id),
+  },
+  (t) => ({
+    spatialIndex: index("spatial_index").using("gist", t.location),
+    regionIndex: index("region_index").on(t.regionId),
+  }),
+);
+
+export const StoreMember = pgTable(
+  "store_member",
+  {
+    userId: text("user_id")
+      .notNull()
+      .references(() => User.id),
+    storeId: text("store_id")
+      .notNull()
+      .references(() => Store.id),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.userId, t.storeId] }),
+  }),
+);
+
+export const StoreMemberRelations = relations(StoreMember, ({ one }) => ({
+  user: one(User, {
+    fields: [StoreMember.userId],
+    references: [User.id],
+  }),
+  store: one(Store, {
+    fields: [StoreMember.storeId],
+    references: [Store.id],
+  }),
+}));
+
+export const StoreRelations = relations(Store, ({ one, many }) => ({
+  storeOwner: one(User, {
+    fields: [Store.ownerId],
+    references: [User.id],
+  }),
+  members: many(StoreMember),
+  products: many(StoreProduct),
+  region: one(Region, {
+    fields: [Store.regionId],
+    references: [Region.id],
+  }),
+}));
+
+export const StoreProduct = pgTable(
+  "store_product",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => createId()),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt", {
+      mode: "date",
+      withTimezone: true,
+    }).$onUpdate(() => new Date()),
+    storeId: text("store_id")
+      .notNull()
+      .references(() => Store.id),
+    shopifyProductId: text("shopify_product_id").notNull(),
+    shopifyVariantId: text("shopify_variant_id").notNull(),
+    // override the product name from shopify
+    // i.e we may have "Banana" as the product name
+    // vendor may want to display "Fresh Local Bananas"
+    displayName: text("display_name"),
+    description: text("description"),
+  },
+  (t) => ({
+    shopifyProductIndex: index("shopify_product_index").on(t.shopifyProductId),
+    shopifyVariantIndex: index("shopify_variant_index").on(t.shopifyVariantId),
+  }),
+);
+
+export const StoreProductRelations = relations(
+  StoreProduct,
+  ({ one, many }) => ({
+    store: one(Store, {
+      fields: [StoreProduct.storeId],
+      references: [Store.id],
+    }),
+    variants: many(StoreProductVariant),
+  }),
+);
+
+/**
+ * Example:
+ * Watermelons can be sold in slices, or wholes
+ * Shopify product: Watermelon (with images and default metadata)
+ * - StoreProduct: Watermelon Whole
+ *  - StoreProductVariant: 500g
+ *  - StoreProductVariant: 1kg
+ */
+
+export const StoreProductVariant = pgTable("store_product_variant", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt", {
+    mode: "date",
+    withTimezone: true,
+  }).$onUpdate(() => new Date()),
+  storeProductId: text("store_product_id")
+    .notNull()
+    .references(() => StoreProduct.id),
+  // can also be "default" for the default variant
+  name: text("name").notNull(),
+  available: boolean("available").default(false),
+  price: decimal("price").notNull(),
+  compareAtPrice: decimal("compare_at_price"),
+  weightInGrams: decimal("weight_in_grams"),
+});
+
+export const StoreProductVariantRelations = relations(
+  StoreProductVariant,
+  ({ one }) => ({
+    storeProduct: one(StoreProduct, {
+      fields: [StoreProductVariant.storeProductId],
+      references: [StoreProduct.id],
+    }),
+  }),
+);
 
 export type Address = typeof Address.$inferSelect;
 export type User = typeof User.$inferSelect;
